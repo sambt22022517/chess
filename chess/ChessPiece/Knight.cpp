4518c2c5965c46741af0e8c38f7cd7b0fd300deb
buisam6565 <buisam6565@gmail.com>
2023-09-12 22:53:50 +0700

    Add validMove() for Kinght

--- chess/ChessPiece/Knight.cpp
+++ chess/ChessPiece/Knight.cpp
@@ -25,8 +25,26 @@ public:
         }
     }
 
-    vector<Point> validMove() override{
-        return {};
+    vector<Point> validMove() override {  
+        vector<Point> director = {Point(2, 1), Point(1, 2), 
+                                  Point(2, -1), Point(1, -2), 
+                                  Point(-2, 1), Point(-1, 2), 
+                                  Point(-2, -1), Point(-1, -2)};
+        vector<Point> output;
+        Point current = coordinate;
+        for(auto i: director){
+            if (Point::checkValid(current.get_i() + i.get_i(), current.get_j() + i.get_j())){
+                ChessPiece* c = dataBoard[current.get_i() + i.get_i()][current.get_j() + i.get_j()];
+                if(c == nullptr){
+                    output.push_back(Point(current.get_i() + i.get_i(), current.get_j() + i.get_j()));
+                } else {
+                    if (c->getkind() != kind) {
+                        output.push_back(Point(current.get_i() + i.get_i(), current.get_j() + i.get_j()));
+                    }
+                }
+            }
+        }
+        return output;
     }
 };
 
